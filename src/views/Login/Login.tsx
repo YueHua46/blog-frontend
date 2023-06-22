308116a16918dd556293ced9a551aa4da0328244
SHENGXI SHI <2766274062@qq.com>
2023-06-22 00:56:27 +0000

    [feat]: 登录界面逻辑初步完成

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,7 +1,48 @@
-import Styles from './Login.module.scss'
 import bgImg from '../../assets/images/login_bg.png'
+import iphoneImg from '../../assets/images/iphone.png'
+import emailImg from '../../assets/images/email.png'
+import React, { useState } from 'react'
 
 export default function Login() {
+  // 登陆状态
+  const [loginEmail, setLoginEmail] = useState('')
+  const [loginPassword, setLoginPassword] = useState('')
+  const [loginTel, setLoginTel] = useState('')
+  const [loginCode, setLoginCode] = useState('')
+  // 当前登陆模式（邮箱或手机号登陆）
+  enum LoginMode {
+    emailMode = 'EmailMode',
+    telMode = 'TelMode',
+  }
+  // 默认模式是email登陆
+  const [loginMode, setLoginMode] = useState(LoginMode.emailMode)
+  // 登陆模式切换
+  function loginModeChange() {
+    if (loginMode === LoginMode.emailMode) setLoginMode(LoginMode.telMode)
+    if (loginMode === LoginMode.telMode) setLoginMode(LoginMode.emailMode)
+  }
+
+  // 邮箱value change事件
+  function emailChange(v: React.FormEvent<HTMLInputElement>) {
+    const value = v.currentTarget.value
+    setLoginEmail(value)
+  }
+  // 手机号value change事件
+  function telChange(v: React.FormEvent<HTMLInputElement>) {
+    const value = v.currentTarget.value
+    setLoginTel(value)
+  }
+  // 密码value change事件
+  function pwChange(v: React.FormEvent<HTMLInputElement>) {
+    const value = v.currentTarget.value
+    setLoginPassword(value)
+  }
+  // 验证码 change事件
+  function codeChange(v: React.FormEvent<HTMLInputElement>) {
+    const value = v.currentTarget.value
+    setLoginCode(value)
+  }
+
   return (
     <section className="flex flex-col md:flex-row h-screen items-center">
       <div className="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
@@ -13,29 +54,51 @@ export default function Login() {
           <h1 className="text-xl md:text-2xl font-bold leading-tight mt-12">
             登录到您的账户
           </h1>
-
-          <form className="mt-6" action="#" method="POST">
+          <form className="mt-6" action="#">
+            {/* 如果是email态就渲染email登陆态，否则是手机号登陆 */}
             <div>
-              <label className="block text-gray-700">账户邮箱</label>
+              <label className="block text-gray-700">
+                {loginMode === LoginMode.emailMode ? '账户邮箱' : '手机号'}
+              </label>
               <input
                 type="email"
-                name=""
-                id=""
-                placeholder="输入账户邮箱"
+                placeholder={
+                  loginMode === LoginMode.emailMode
+                    ? '输入账户邮箱'
+                    : '输入手机号'
+                }
+                value={
+                  loginMode === LoginMode.emailMode ? loginEmail : loginTel
+                }
+                onInput={
+                  loginMode === LoginMode.emailMode ? emailChange : telChange
+                }
                 className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
                 autoFocus
                 autoComplete="on"
                 required
               />
             </div>
+
             <div className="mt-4">
-              <label className="block text-gray-700">密码</label>
+              <label className="block text-gray-700">
+                {loginMode === LoginMode.emailMode ? '密码' : '验证码'}
+              </label>
               <input
                 type="password"
                 name=""
                 id=""
-                placeholder="输入密码"
-                minLength={6}
+                placeholder={
+                  loginMode === LoginMode.emailMode
+                    ? '请输入密码'
+                    : '请输入验证码'
+                }
+                onInput={
+                  loginMode === LoginMode.emailMode ? pwChange : codeChange
+                }
+                value={
+                  loginMode === LoginMode.emailMode ? loginPassword : loginCode
+                }
                 className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
                 required
               />
@@ -61,39 +124,18 @@ export default function Login() {
           <button
             type="button"
             className="w-full block bg-white hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300">
-            <div className="flex items-center justify-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                xmlnsXlink="http://www.w3.org/1999/xlink"
+            <div
+              className="flex items-center justify-center"
+              onClick={loginModeChange}>
+              <img
+                src={loginMode === LoginMode.emailMode ? iphoneImg : emailImg}
                 className="w-6 h-6"
-                viewBox="0 0 48 48">
-                <defs>
-                  <path
-                    id="a"
-                    d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"
-                  />
-                </defs>
-                <clipPath id="b">
-                  <use xlinkHref="#a" overflow="visible" />
-                </clipPath>
-                <path clipPath="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" />
-                <path
-                  clipPath="url(#b)"
-                  fill="#EA4335"
-                  d="M0 11l17 13 7-6.1L48 14V0H0z"
-                />
-                <path
-                  clipPath="url(#b)"
-                  fill="#34A853"
-                  d="M0 37l30-23 7.9 1L48 0v48H0z"
-                />
-                <path
-                  clipPath="url(#b)"
-                  fill="#4285F4"
-                  d="M48 48L17 24l-4-3 35-10z"
-                />
-              </svg>
-              <span className="ml-4">Google登录</span>
+              />
+              <span className="ml-4">
+                {loginMode === LoginMode.emailMode
+                  ? '手机验证码登陆'
+                  : '账户邮箱登陆'}
+              </span>
             </div>
           </button>
 
